b43de4a1b4890e93d75eb3f9f5e91ed751101b44
Dmitriy <rozandima01@mail.ru>
2025-06-09 22:57:18 +0300

    09.06.25 Собеседование RangeVision

---
+++ RangeVision/RangeVision/RangeVision.cpp
@@ -0,0 +1,57 @@
+﻿// RangeVision.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <vector>
+
+int main()
+{
+    int N = 50;
+
+    std::vector<int> p_vec;
+
+    if (N > 1) { p_vec.push_back(2); }
+    for (int i = 3; i < N; i += 2)
+    {
+        bool is_simple = true;
+        for (int p : p_vec)
+        {
+            if (p * p > i) { break; } // p > sqrt(i)
+
+            if (i % p == 0)
+            {
+                is_simple = false;
+                break;
+            }
+        }
+
+        if (is_simple) { p_vec.push_back(i); }
+    }
+
+    for (int num : p_vec) { std::cout << num << " "; }   
+}
+
+//int main()
+//{
+//    int N = 50;
+//
+//    std::vector<int> p_vec;
+//
+//    if (N > 1) { std::cout << 2 << " "; }
+//    for (int i = 3; i < N; i += 2)
+//    {
+//        bool is_simple = true;
+//        for (int j = 3; j <= std::sqrt(i); j += 2)
+//        {
+//            if (i % j == 0)
+//            {
+//                is_simple = false;
+//                break;
+//            }
+//        }
+//
+//        if (is_simple) { p_vec.push_back(i); }
+//    }
+//
+//    for (int num : p_vec) { std::cout << num << " "; }
+//}
